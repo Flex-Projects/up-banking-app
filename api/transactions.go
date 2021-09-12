be0dc9cb2e018f1798fd27e62987828f00d58f93
Ellis Johnson <ellisjohnson111@gmail.com>
2021-09-12 16:45:21 +1000

    Committing Test json and API call

---
+++ api/transactions.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	client := &http.Client{
+		CheckRedirect: http.DefaultClient.CheckRedirect,
+	}
+
+	req, err := http.NewRequest("GET", "https://api.up.com.au/api/v1/transactions", nil)
+	req.Header.Add("Authorization", `Bearer up:yeah:0YLVPc93cixD2ElxmAEtKBJYqaNvyTFrIBoyjuZWBy4c0EZZ8EWIyulytLGw6LiWM0XD8hQJZVoP1sOtzPjs2373DqLfQCVVofeUKAifZlBi5KIXafbHNmNdW1PPuoh5`)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+
+}
